5312324c32159f8c53755d6e71f8d5974b7c3ea1
Marta2852 <ipa23.m.dzerve@vtdt.edu.lv>
2025-12-05 09:16:15 +0200

    Pievienot Printfulify logo

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <p>Printfulify is a sample application demonstrating the integration of Printful's API with a PHP backend. 
         It showcases how to manage products, orders, and other functionalities provided by Printful.</p>
+
+    <img src=Printfulify_Logo.png alt="Printfulify Logo" />
+    <p>Izdrukā rītdienu šodien. </p>
 </body>
 </html>
