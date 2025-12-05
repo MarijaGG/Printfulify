69578945ae1fb316ca85deab655da94c0def8fae
MarijaGG <ipa23.m.gutane@vtdt.edu.lv>
2025-12-05 08:49:37 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Printfulify</title>
+</head>
+<body>
+    <p>Printfulify is a sample application demonstrating the integration of Printful's API with a PHP backend. 
+        It showcases how to manage products, orders, and other functionalities provided by Printful.</p>
+</body>
+</html>
